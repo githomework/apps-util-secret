7ec42cc5b2628b21374eb385af1b462578ca40a0
David Lam <david.lam@winc.com.au>
2020-10-07 12:45:04 +1100

    KeyOutputPair

--- random.go
+++ random.go
@@ -1,7 +1,10 @@
 package secret
 
 import (
+	"encoding/json"
+	"fmt"
 	"math/rand"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -12,12 +15,33 @@ func Random16() string {
 	rand.Seed(time.Now().Unix())
 
 	var output strings.Builder
-
+	output.Grow(length)
 	for i := 0; i < length; i++ {
 		random := rand.Intn(len(charSet))
 		randomChar := charSet[random]
 		output.WriteString(string(randomChar))
 	}
 	return output.String()
+
+}
+
+func KeyOutputPair(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type submitType struct {
+		Text string `json:"text"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+
+	var t submitType
+	err := decoder.Decode(&t)
+	if err != nil {
+		fmt.Fprint(w, `{"key":"Problem encountered. See log."}`)
+		return
+	}
+	key := Random16()
+	output := Encrypt(key, t.Text)
+
+	fmt.Fprint(w, `{"key":"`+key+`","output":"`+output+`"}`)
 
 }
