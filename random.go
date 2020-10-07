f0d299fb3711dbba810c1fd736edd80a4c62a45f
David Lam <david.lam@winc.com.au>
2020-10-07 16:44:41 +1100

    used crypto/rand

--- random.go
+++ random.go
@@ -1,24 +1,27 @@
 package secret
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"math/rand"
+	"math"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func Random16() string {
 	const length = 16
 	const charSet = "0123456789abcdefghijk-mnopqrstvwxyz" // not have lower case L to minimise confusion
-	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "0000000000000000"
+	}
 
 	var output strings.Builder
 	output.Grow(length)
 	for i := 0; i < length; i++ {
-		random := rand.Intn(len(charSet))
-		randomChar := charSet[random]
+		randomChar := charSet[int(math.Abs(float64(int(b[i]))))%35]
 		output.WriteString(string(randomChar))
 	}
 	return output.String()
